2cb377726fe75479525d6350f4af35270f317ceb
Kelvin <kelvinsb52@gmail.com>
2025-01-19 16:03:09 -0500

    Models and Schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
+
 
 const router = express.Router()
 
@@ -15,9 +17,18 @@ router.get('/:id', (req, res) =>
 })
 
 // POST a new workout
-router.post('/', (req, res) => 
+router.post('/', async (req, res) => 
 {
-    res.json({mssg: 'POST a new workout'})
+    const {title, load, reps} = req.body
+
+    try
+    {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch(error)
+    {
+        res.status(400).json({error: error.message})
+    }
 })
 
 // DELETE a new workout
