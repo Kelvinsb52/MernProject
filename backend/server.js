806d93eee828cf14bd9719e6938a066cc0449975
Kelvin <kelvinsb52@gmail.com>
2025-01-19 15:45:16 -0500

    MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // Express App
@@ -17,9 +18,21 @@ app.use((req, res, next) =>
 
 // routes
 app.use('/api/workouts', workoutRoutes)
-// Listen for requests
-app.listen(process.env.PORT, () =>
-{
-    console.log('listening on port', process.env.PORT)
-}) 
+
+// Connect to Database
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> 
+    {
+        // Listen for requests
+        app.listen(process.env.PORT, () =>
+        {
+        console.log('Connected to DB & Listening on Port', process.env.PORT)
+        }) 
+    
+    
+    })
+    .catch((error) => 
+    {
+        console.log(error)
+    })
 
